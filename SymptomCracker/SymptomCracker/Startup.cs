4c65af80f2f13f5029d5b99e4a4d73e7ac7112f8
BlueCandleGames <bluecandlegames@gmail.com>
2016-09-21 21:12:41 -0400

    Initial project creation.

---
+++ SymptomCracker/SymptomCracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SymptomCracker.Startup))]
+namespace SymptomCracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
